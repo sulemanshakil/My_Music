8783bd926fb3e2b9131b3df42b8f6c79395b902e
sulemanshakil <bilorani1>
2016-06-18 22:34:30 +0200

    ini

--- app/src/main/java/com/example/android/my_music/MainActivity.java
+++ app/src/main/java/com/example/android/my_music/MainActivity.java
@@ -64,7 +64,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         setSupportActionBar(toolbar);
         songList = new ArrayList<>();
         getSongList();
-        populateSongs();
+        populateSongs();  //pop
 
         final DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
